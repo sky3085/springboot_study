f416ec3ac3a8eb3de17d29742a174e13715c21f0
yeonghwan Moon <skysky3085@daum.net>
2022-07-26 23:16:25 +0900

    스프링부트 연습 프로젝트

---
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -0,0 +1,41 @@
+package hello.hellospring.service;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    public Long join(Member member) {
+
+        //중복회원이 있는가!? 같은 이름 안됨
+        validateDuplicateMember(member);
+
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName()).ifPresent(m ->{//Optional 이기 떄문에 null이 아니니까!
+            throw new IllegalStateException("Alreay Exist");
+        });
+    }
+
+    //전체 회원 조회
+    public List<Member> findMember() {
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
